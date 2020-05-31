541ab4109086c67f73d2a09adf96beb18fc26120
Joshua.Genders <joshuagenders@hotmail.com>
2020-05-31 23:43:01 +0100

    add timing to example

--- Lupi.Examples/PuppeteerExample.cs
+++ Lupi.Examples/PuppeteerExample.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using System;
+using System.Diagnostics;
 using System.Net;
 using System.Threading.Tasks;
 
@@ -7,14 +8,18 @@ namespace Lupi.Examples
 {
     public class PuppeteerExample
     {
-        public async Task Homepage()
+        public async Task<TimeSpan> Homepage()
         {
             using (var page = await GlobalState.Browser.NewPageAsync())
             {
+                var watch = new Stopwatch();
+                watch.Start();
                 await page.GoToAsync("https://blazedemo.com/");
                 await page.WaitForSelectorAsync("div.container");
+                watch.Stop();
                 var title = await page.EvaluateExpressionAsync<dynamic>("document.getElementsByTagName('h1')[0].innerText");
                 title.Should().Be("Welcome to the Simple Travel Agency!");
+                return watch.Elapsed;
             }
         }
     }
