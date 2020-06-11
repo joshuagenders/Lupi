99dbfba265ef008e2d329aa607b84781f0fdf454
Joshua.Genders <joshuagenders@hotmail.com>
2020-06-11 20:43:19 +0100

    formatting

--- Lupi/Listeners/ConsoleAggregatorListener.cs
+++ Lupi/Listeners/ConsoleAggregatorListener.cs
@@ -13,10 +13,11 @@ namespace Lupi.Listeners
         }
         public async Task OnResult(AggregatedResult result, CancellationToken ct)
         {
+            Console.WriteLine("----------------");
             Console.WriteLine($"Moving Average: {result.MovingAverage}ms, Min: {result.Min}ms, Max: {result.Max}ms");
             Console.WriteLine($"Period Average: {result.PeriodAverage}ms, Period Min: {result.PeriodMin}ms, Period Max: {result.PeriodMax}ms");
             Console.WriteLine($"Period Length: {result.PeriodLength}ms, Sample Count: {result.Count}");
-            Console.WriteLine($"Success:{result.PeriodSuccessCount} Error: {result.PeriodErrorCount}.");
+            Console.WriteLine($"Success: {result.PeriodSuccessCount} Error: {result.PeriodErrorCount}.");
             await Task.CompletedTask;
         }
     }
