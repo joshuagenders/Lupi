2e90b53cea68677b247bda7a274a16ceec104f10
Joshua.Genders <joshuagenders@hotmail.com>
2020-06-13 14:05:23 +0100

    tests

--- Lupi.Tests/ConfigurationValidationTests.cs
+++ Lupi.Tests/ConfigurationValidationTests.cs
@@ -10,7 +10,7 @@ namespace Lupi.Tests
     public class ConfigurationValidationTests
     {
         #region ConfigStrings
-        private const string AllConfigBasic = @"
+        private const string ConfigBasic = @"
 test:
     assemblyPath: path/to/my.dll
     singleTestClassInstance: true
@@ -35,7 +35,7 @@ throughput:
     rampDown: 2m
     thinkTime: 500ms
 ";
-        private const string AllConfigPhases = @"
+        private const string ConfigPhases = @"
 test:
     assemblyPath: path/to/my.dll
     singleTestClassInstance: true
@@ -72,11 +72,43 @@ throughput:
         from: 30
         to: 0
 ";
+        private const string ConfigMinimal = @"
+test:
+    assemblyPath: path/to/my.dll
+    testClass: MyNamespace.MyClass # optional if method name is unique in the assembly
+    testMethod: MyMethod
+concurrency:
+    threads: 10 
+throughput:
+    thinkTime: 1s
+";
+
+        private const string InvalidNegativeValues = @"
+test:
+    assemblyPath: path/to/my.dll
+    singleTestClassInstance: true
+    testClass: MyNamespace.MyClass # optional if method name is unique in the assembly
+    testMethod: MyMethod
+concurrency:
+    threads: -4
+    openWorkload: true # (can add additional threads when throughput is not met)
+    minThreads: -3 # requires open workload
+    maxThreads: -15 # requires open workload
+throughput:
+    tps: -20
+    holdFor: 10m
+    thinkTime: 500ms
+";
+        private const string InvalidEmpty = @"
+test:
+    assemblyPath: path/to/my.dll
+";
 
         #endregion
         [Theory]
-        [InlineData(AllConfigBasic)]
-        [InlineData(AllConfigPhases)]
+        [InlineData(ConfigBasic)]
+        [InlineData(ConfigPhases)]
+        [InlineData(ConfigMinimal)]
         public async Task WhenBasicConfigurationIsValidated_ThenValidationPasses(string config)
         {
             var validator = new ConfigurationValidator();
@@ -84,6 +116,18 @@ throughput:
             var result = validator.Validate(parsed);
             result.Errors.Should().BeEmpty();
             result.IsValid.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(InvalidNegativeValues)]
+        [InlineData(InvalidEmpty)]
+        public async Task WhenBasicConfigurationIsValidated_ThenValidationFails(string config)
+        {
+            var validator = new ConfigurationValidator();
+            var parsed = await ConfigHelper.GetConfigFromString(config);
+            var result = validator.Validate(parsed);
+            result.Errors.Should().NotBeEmpty();
+            result.IsValid.Should().BeFalse();
         }
     }
 }
