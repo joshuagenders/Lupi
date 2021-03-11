4ddb007451c42cfaa98413e0dc92ce12b1d93606
Joshua Genders <joshuagenders@hotmail.com>
2021-03-11 19:36:25 +0000

    set batch size at start of loop

--- Lupi/TestResultPublisher.cs
+++ Lupi/TestResultPublisher.cs
@@ -39,7 +39,7 @@ namespace Lupi
         {
             while (!ct.IsCancellationRequested && !TestCompleted || _results.Any())
             {
-                var resultProcessingBatchSize = 5000;
+                var resultProcessingBatchSize = _results.Count;
                 var count = 0;
                 var batch = new List<TestResult>();
                 while (_results.TryDequeue(out var result) && count < resultProcessingBatchSize)
