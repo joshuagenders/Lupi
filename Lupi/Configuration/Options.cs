ca29ea783b566592b884e443e0c17ffcac9a69a9
Joshua Genders <joshuagenders@hotmail.com>
2021-03-18 22:08:39 +0000

    wip

--- Lupi/Configuration/Options.cs
+++ Lupi/Configuration/Options.cs
@@ -1,4 +1,6 @@
-﻿using CommandLine;
+﻿using System;
+using System.Collections.Generic;
+using CommandLine;
 
 namespace Lupi.Configuration
 {
@@ -6,5 +8,30 @@ namespace Lupi.Configuration
     {
         [Value(1, Required = true, HelpText = "The configuration file path")]
         public string ConfigFilepath { get; set; }
+
+        [Option('f', "file", HelpText = "The filepath of the .DLL containing the test.")]
+        public int TestFile { get; set; }
+
+        [Option('t', "test", HelpText = "The fully qualified class and method name. E.g. 'MyNamespace.MyClassName::MyMethod'")]
+        public int Test { get; set; }
+
+        [Option('c', "concurrency", HelpText = "The number of concurrent tests.")]
+        public int Concurrency { get; set; }
+
+        [Option('r', "throughput", HelpText = "The number of tests per second.")] 
+        public double Tps { get; set; }
+
+        [Option('w', "ramp-up", HelpText = "Ramp-up seconds.")]
+        public int RampUpMinutes { get; set; }
+
+        [Option('d', "duration", HelpText = "Duration seconds.")]
+        public int DurationSeconds { get; set; }
+
+        [Option('s', "ramp-down", HelpText = "Ramp-down seconds.")]
+        public int RampDownMinutes { get; set; }
+
+        [Option('e', "exit-condition", HelpText = "Comma separated list of Exit condition(s).")]
+        public IEnumerable<string> ExitConditions { get; set; }
+
     }
 }
