1f32c11513e737aaec4951dd1c224425e528c28f
Joshua Genders <joshuagenders@hotmail.com>
2021-05-08 21:43:01 +0100

    test placeholders

--- Lupi.Tests/ApplicationTests.cs
+++ Lupi.Tests/ApplicationTests.cs
@@ -4,17 +4,14 @@ namespace Lupi.Tests
 {
     public class ApplicationTests 
     {
-        // todo
-        // Running application responds to exit signal
-        // 		_testResultPublisher.TestCompleted is true
-        // 		test passed/failed is reflected
-        // 	Application runs publishers and aggregators
-        // 	publishers and aggregators are awaited after test completion
-        // 		testcompleted is set
-
-        public void Test(){
-
+        public void WhenExitSignalled_ThenApplicationCompletes()
+        {
+            throw new NotImplementedException();
+            // _testResultPublisher.TestCompleted is true
+            // test passed/failed is reflected
+            // Application runs publishers and aggregators
+            //	publishers and aggregators are awaited after test completion
+            // test completed is set
         }
-
     }
 }
