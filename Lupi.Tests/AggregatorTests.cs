853b3cdbd1c37c23a04def98dd0c6967b60e64a9
Joshua Genders <joshuagenders@hotmail.com>
2021-05-08 22:06:02 +0100

    test

--- Lupi.Tests/AggregatorTests.cs
+++ Lupi.Tests/AggregatorTests.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Lupi.Listeners;
+using Moq;
 using Xunit;
 
 namespace Lupi.Tests 
@@ -6,13 +10,37 @@ namespace Lupi.Tests
     public class AggregatorTests 
     {
         // todo
-        [Fact]
-        public void WhenSingleResultAggregated_ThenResultIsUnchanged()
+        [Theory]
+        [AutoMoqData]
+        public async Task WhenSingleResultAggregated_ThenResultIsUnchanged(
+            Mock<IAggregatorListener> aggregatorListener,
+            Aggregator aggregator)
         {
-            throw new NotImplementedException();
+            var cts = new CancellationTokenSource();
+            var duration = TimeSpan.FromMilliseconds(2341);
+            aggregator.Subscribe(aggregatorListener.Object);
+            await aggregator.OnResult(
+                new Results.TestResult[] {
+                    new Results.TestResult {
+                        Duration = duration,
+                        FinishedTime = DateTime.Now,
+                        Passed = true,
+                        ThreadName = "threadname"
+                    }
+                }, cts.Token);
+            var processTask = aggregator.Process(cts.Token);
+            cts.CancelAfter(100);
+            await processTask;
+            aggregatorListener.Verify(a => a.OnResult(It.Is<AggregatedResult>(r => 
+                r.Count == 1
+                && r.Max == duration.TotalMilliseconds
+                && r.Mean == duration.TotalMilliseconds
+                && r.Min == duration.TotalMilliseconds
+                && r.PeriodSuccessCount == 1
+            ), It.IsAny<CancellationToken>()));
         }
 
-        [Theory]
+        [Fact]
         public void WhenManyResultsAggregated_ThenListenersAreSentCorrectMetrics()
         {
             throw new NotImplementedException();
