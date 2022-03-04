6c89c3533941ae95e74aa076e09216aa2e0632a1
Joshua Genders <joshuagenders@hotmail.com>
2022-03-05 07:07:30 +1000

    allow async to transfer control to sut

--- Lupi.Tests/ThreadMarshallTests.cs
+++ Lupi.Tests/ThreadMarshallTests.cs
@@ -1,19 +1,19 @@
 using Lupi.Configuration;
 using Lupi.Core;
 
-namespace Lupi.Tests 
+namespace Lupi.Tests
 {
-    public class ThreadMarshallTests 
+    public class ThreadMarshallTests
     {
         [Theory]
         [AutoMoqData]
-        public void WhenConcurrencySpecifiedInClosedWorkload_ThenThreadLevelSet(
-            [Frozen]Mock<ITokenManager> tokenManager,
-            [Frozen]Config config,
+        public async void WhenConcurrencySpecifiedInClosedWorkload_ThenThreadLevelSet(
+            [Frozen] Mock<ITokenManager> tokenManager,
+            [Frozen] Config config,
             ThreadMarshall threadMarshall)
         {
             var cts = new CancellationTokenSource();
-            var startTime = new DateTime(2020,05,02, 13,0,0);
+            var startTime = new DateTime(2020, 05, 02, 13, 0, 0);
             var endTime = startTime.AddMinutes(5);
             config.Concurrency.RampUp = TimeSpan.FromMinutes(1);
             config.Concurrency.Threads = 10;
@@ -22,12 +22,15 @@ namespace Lupi.Tests
             config.Concurrency.OpenWorkload = false;
             config.Concurrency.Phases = config.BuildStandardConcurrencyPhases();
             threadMarshall.AdjustThreadLevels(startTime, startTime.AddSeconds(30), cts.Token);
+            await Task.Delay(1);
             threadMarshall.GetThreadCount().Should().BeCloseTo(config.Concurrency.Threads / 2, 1);
 
             threadMarshall.AdjustThreadLevels(startTime, startTime.AddMinutes(2), cts.Token);
+            await Task.Delay(1);
             threadMarshall.GetThreadCount().Should().BeCloseTo(config.Concurrency.Threads, 1);
 
             threadMarshall.AdjustThreadLevels(startTime, startTime.AddMinutes(4).AddSeconds(30), cts.Token);
+            await Task.Delay(1);
             tokenManager.Verify(
                 t => t.RequestTaskDiscontinues(),
                 Times.Between((int)(config.Concurrency.Threads / 2 - 1), (int)(config.Concurrency.Threads / 2 + 1), Moq.Range.Inclusive));
@@ -35,15 +38,15 @@ namespace Lupi.Tests
 
         [Theory]
         [AutoMoqData]
-        public void WhenConcurrencySpecifiedInOpenWorkload_ThenThreadLevelAdjusts(
-            [Frozen]Mock<ITestThreadFactory> testThreadFactory,
+        public async Task WhenConcurrencySpecifiedInOpenWorkload_ThenThreadLevelAdjusts(
+            [Frozen] Mock<ITestThreadFactory> testThreadFactory,
             Mock<TestThread> testThread,
-            [Frozen]Mock<ITokenManager> tokenManager,
-            [Frozen]Config config,
+            [Frozen] Mock<ITokenManager> tokenManager,
+            [Frozen] Config config,
             ThreadMarshall threadMarshall)
         {
             var cts = new CancellationTokenSource();
-            var startTime = new DateTime(2020,05,02, 13,0,0);
+            var startTime = new DateTime(2020, 05, 02, 13, 0, 0);
             var endTime = startTime.AddMinutes(5);
             config.Concurrency.OpenWorkload = true;
             config.Concurrency.MinThreads = 10;
@@ -54,10 +57,12 @@ namespace Lupi.Tests
             testThread.Setup(t => t.Run(It.IsAny<CancellationToken>())).Returns(Task.Delay(TimeSpan.FromSeconds(30)));
             testThreadFactory.Setup(t => t.GetTestThread()).Returns(testThread.Object);
             threadMarshall.AdjustThreadLevels(startTime, startTime, cts.Token);
+            await Task.Delay(1);
             threadMarshall.GetThreadCount().Should().Be(10);
 
             tokenManager.Setup(t => t.GetTokenCount()).Returns(1);
             threadMarshall.AdjustThreadLevels(startTime, startTime.AddSeconds(10), cts.Token);
+            await Task.Delay(1);
             threadMarshall.GetThreadCount().Should().Be(11);
         }
     }
